363748aaacc5ca0da89eba5a4f9a8764ded5e911
peppaoo <lucascvgomes15@gmail.com>
2025-09-27 00:45:24 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,IonContent, IonFooter, IonHeader, IonTitle, IonToolbar  } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule],
+})
+export class HomePage {
+  constructor() {}
+}
